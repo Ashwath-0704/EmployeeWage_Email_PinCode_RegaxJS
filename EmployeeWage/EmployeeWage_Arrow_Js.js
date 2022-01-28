78ecc00fd11bbd8e0166ed498268d7d419e2b2d8
Achyuth Naidu <ashwath.bly@gamil.com>
2022-01-28 13:16:58 +0530

    Added : calculate wage using Arrow function

---
+++ EmployeeWage/EmployeeWage_Arrow_Js.js
@@ -0,0 +1,14 @@
+class EmployeeWage {
+    id;
+    salary;
+    gender;
+    startDate;
+    constructor(...paramas) {
+        this.id = paramas[0];
+        this.id = paramas[0];
+        this.id = paramas[0];
+        this.id = paramas[0];
+        this.id = paramas[0];
+        this.id = paramas[0];
+    }
+}
